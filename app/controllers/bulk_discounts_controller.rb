43998607198ea335d5f7236441d50019bffcaf08
Erika Kischuk <83726180+eakischuk@users.noreply.github.com>
2021-09-26 18:22:34 -0600

    Revert "Story1"

--- app/controllers/bulk_discounts_controller.rb
+++
@@ -1,9 +0,0 @@
-class BulkDiscountsController < ApplicationController
-  def index
-    @discounts = BulkDiscount.all
-  end
-
-  def show
-    @discount = BulkDiscount.find(params[:id])
-  end
-end
