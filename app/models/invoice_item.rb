4fa50239c987788dec441823d5de8dbd5f94bc55
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-28 11:30:14 -0600

    Add best discount and revenue methods

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,6 +3,8 @@ class InvoiceItem < ApplicationRecord
 
   belongs_to :item
   belongs_to :invoice
+  has_one :merchant, through: :item
+  has_many :bulk_discounts, through: :merchant
 
   def item_name
     item.name
@@ -10,5 +12,23 @@ class InvoiceItem < ApplicationRecord
 
   def formatted_date
     invoice.formatted_date
+  end
+
+  def best_discount
+    bulk_discounts.where('bulk_discounts.quantity_threshold <= ?', quantity)
+    .order(percentage: :desc)
+    .first
+  end
+
+  def total_revenue
+    quantity * unit_price
+  end
+
+  def discounted_revenue
+    if best_discount.nil?
+      total_revenue
+    else
+      total_revenue * best_discount.remaining_percentage
+    end
   end
 end
