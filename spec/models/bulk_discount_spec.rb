aaa73e6b921290f7afbab199ae5abb23afb17d0a
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-28 11:29:11 -0600

    Add test for remaining_percentage instance method

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
+  before(:each) do
+    @bulk_discount_1 = create(:bulk_discount) # 15% for 5 items
+  end
   describe 'relationships' do
     it { should belong_to(:merchant) }
   end
@@ -8,5 +11,11 @@ RSpec.describe BulkDiscount, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:percentage) }
     it { should validate_presence_of(:quantity_threshold) }
+  end
+
+  describe 'instance methods' do
+    it 'returns remaining_percentage after discount' do
+      expect(@bulk_discount_1.remaining_percentage).to eq(0.85)
+    end
   end
 end
